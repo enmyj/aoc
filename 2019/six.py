6988ccd18d062b7fe69ac525ca611d5781da69b5
Ian <imyjer@gmail.com>
2019-12-15 21:54:17 -0500

    networkx ftw

--- 2019/six.py
+++ 2019/six.py
@@ -20,46 +20,50 @@ def read_input(inpt: str, file: bool = True) -> Objs:
     return [ln.strip().split(')') for ln in lns]
 
 
-def orbits(objs: Objs) -> int:
+def num_orbitals(objs: Objs) -> int:
+    """
+    """
+    # create graph from input
+    g = nx.DiGraph()
+    g.add_edges_from(objs)
+
+    # list of unique planets
+    planets = list(g.nodes)
+    planets.remove('COM')
+
+    # lol there's gotta be a smarter way to do this
+    cnt = 0
+    for planet in planets:
+        pred = list(g.predecessors(planet))[0]
+        cnt += 1
+        while pred != 'COM':
+            pred = list(g.predecessors(pred))[0]
+            cnt += 1
+
+    return cnt
+
+
+def shortest_path_to_santa(objs: Objs) -> int:
     """
     """
     # create graph from input
     g = nx.Graph()
     g.add_edges_from(objs)
 
-    # get list of unique planets
-    l, r = [], []
-    for i in objs:
-        l.append(i[0])
-        r.append(i[1])
-    all_planets = set(l + r)
-
-
-    # find number of edges to left of each planet
-    # ha, apparently graph can go in any direction
-    ap_no_com = list(all_planets - set(['COM']))
-    cnt = 0
-    for planet in ap_no_com:
-        left = list(g.neighbors(planet))[0]
-        cnt += 1
-        while left != 'COM':
-            left = list(g.neighbors(left))[0]
-            cnt += 1
-    
-    return cnt
+    return nx.shortest_path_length(g, 'YOU', 'SAN') - 2
 
 
 
 def q6a(objs: Objs) -> int:
     """
     """
-    return orbits(objs)
+    return num_orbitals(objs)
 
 
-def q6b(input_file: str) -> int:
+def q6b(objs: Objs) -> int:
     """
     """
-    pass
+    return shortest_path_to_santa(objs)
 
 
 if __name__ == "__main__":
@@ -77,10 +81,26 @@ if __name__ == "__main__":
     J)K
     K)L"""
     
-    # assert orbits(read_input(test_input, file = False)) == 42
+    assert num_orbitals(read_input(test_input, file = False)) == 42
+
+    test_input2 = """COM)B
+    B)C
+    C)D
+    D)E
+    E)F
+    B)G
+    G)H
+    D)I
+    E)J
+    J)K
+    K)L
+    K)YOU
+    I)SAN"""
+
+    assert shortest_path_to_santa(read_input(test_input2, file = False)) == 4
 
 
     # answers
     fp = '/home/imyjer/repos/aoc/2019/data/six.input'
     print(q6a(read_input(fp)))
-    # print(q6b(fp))
+    print(q6b(read_input(fp)))
